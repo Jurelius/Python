1eff9517cc8d4a8d56ee256ca64499d057dcef95
Cristian Morales <jurelius@gmail.com>
2024-03-13 19:28:08 -0300

    guarda ejercicio 4

---
+++ dia4/ejercicio4.py
@@ -0,0 +1,6 @@
+""" 
+solicitar en pseudocodigo al usuario en ingreso de 3 numeros y 
+determinar cuales de ellos es mayor que 33
+solo numeros enteros 1 al 100
+y determinar cual es mayor y cual es menor
+"""
--- dia5/ejercicio5.py
+++ dia5/ejercicio5.py
@@ -1,17 +1,17 @@
 """ Realizar un pseudocódigo que permita determinar el área de un triángulo
     rectángulo si el usuario ingresa los valores como la base y la altura del triángulo. 
     
-    Solicitar a usuario ingresar base y altura del triángulo rectangulo
+    --Solicitar a usuario ingresar base y altura del triángulo rectangulo
     
     escribir "Ingresar base del triángulo"
     leer base
     escribir "ingresar altura del triángulo"
     leer altura
     
-    cálcular area del triangulo rectángulo con la formula
+    --cálcular area del triangulo rectángulo con la formula
     area = (base * altura) / 2
     
-    mostrar resultado al usuario
+    --mostrar resultado al usuario
     
     Escribir "El area del triángulo es:" area
     
@@ -19,6 +19,42 @@
     
     
     
+    Desarrollar un pseudocódigo que permita calcular el área de una circunferencia
+    partiendo de la fórmula: área = 𝝥*radio2.
+    (La constante pi tiene un valor aproximado de 3,14).
     
+    
+    --Solicitar a usuario ingresar el radio de la circunferencia
+    escribir "Ingresar radio de la circunferencia"
+    leer radio
+        
+    --cálcular area de la circunferencia con la siguiente formula
+    area = (3.14 * r * r)
+    
+    --mostrar resultado al usuario
+    
+    Escribir "El area de la circunferencia:" area
+    
+    Fin
+    
+    Diseñar un pseudocódigo para calcular el área de un rombo, en donde el área es se
+    calcula mediante la fórmula:
+    área = (Diagonal mayor * diagonal menor)/2
+    
+    --Solicitar a usuario ingresar diagonal mayor y diagonal menor del rombo
+    
+    escribir "Ingresar diagonal mayor del rombo"
+    leer diagonal mayor
+    escribir "ingresar diagonal menor del rombo"
+    leer diagonal menor
+    
+    --cálcular area del rombo con la formula
+    area = (diagonal mayor * diagonal menor) / 2
+    
+    --mostrar resultado al usuario
+    
+    Escribir "El area del rombo es:" area
+    
+    Fin
     
 """
