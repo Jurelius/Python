d82c053809e7de0b49ccc65291f6e822d9700a6d
Cristian Morales <jurelius@gmail.com>
2024-03-13 18:41:49 -0300

    se genera pseudocódigo cálculo area de un triángulo rectángulo

---
+++ dia5/ejercicio5.py
@@ -0,0 +1,24 @@
+""" Realizar un pseudocódigo que permita determinar el área de un triángulo
+    rectángulo si el usuario ingresa los valores como la base y la altura del triángulo. 
+    
+    Solicitar a usuario ingresar base y altura del triángulo rectangulo
+    
+    escribir "Ingresar base del triángulo"
+    leer base
+    escribir "ingresar altura del triángulo"
+    leer altura
+    
+    cálcular area del triangulo rectángulo con la formula
+    area = (base * altura) / 2
+    
+    mostrar resultado al usuario
+    
+    Escribir "El area del triángulo es:" area
+    
+    Fin
+    
+    
+    
+    
+    
+"""
