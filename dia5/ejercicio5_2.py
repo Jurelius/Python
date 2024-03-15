5b646e5e741807374ecf1c6f1bcf307083b01876
Cristian Morales <jurelius@gmail.com>
2024-03-15 18:06:03 -0300

    ejercicios dia 5

---
+++ dia5/ejercicio5_2.py
@@ -0,0 +1,41 @@
+edad = 27
+#¿Juán es mayor de edad?
+print(edad >= 18)#True
+print(edad < 18) #False
+
+edad_graduacion_colegio = 17
+#¿Juan se graduó antes de los 18 años?
+print(edad_graduacion_colegio < 18)#True
+print(edad_graduacion_colegio >= 18)#False
+
+experiencia_laboral = 4
+#¿Juan no tiene 4 años de experiencia laboral?
+print(experiencia_laboral != 4)#False
+print(experiencia_laboral == 4)#True
+
+numero_hijos = 0
+#¿Juán tiene hijos?
+print(numero_hijos > 0)# False
+print(numero_hijos == 0)# True
+print(numero_hijos < 1)# True
+print(numero_hijos !=0)# False
+print(numero_hijos >= 1)# False
+
+"""
+1.-Si las exportaciones disminuyen entonces bajarán las utilidades
+2.-Los precios son altos si y sólo sí los costos aumentan
+3.-Si la producción aumenta entonces bajarán los precios
+4.-Si la contaminación aumenta entonces existirá restricción vehicular adicional
+"""
+
+nombre = "Juan"
+me_llamo_juan = (nombre == "Juan")
+
+#print(type(me_llamo_juan)) sirve para ver el tipo de variable , en este caso bool
+
+#Lógica proposicional
+#Quieres helado  (P)
+#Quieres bebida  (Q)
+
+AND (Y; i)
+
