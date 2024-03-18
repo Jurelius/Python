f8a03273cecd566e245b4bd4c57c578ce96cd45d
Cristian Morales <jurelius@gmail.com>
2024-03-17 22:32:23 -0300

    Se desarrolla desafio cálculo IMC

--- dia6/desafio1/imc.py
+++ dia6/desafio1/imc.py
@@ -1,14 +1,18 @@
+kilogramos = float(input("Ingrese su peso en Kilógramos\n"))
+estatura = float(input ("Ingrese su estatura en centímetros\n"))/100 #transforma centímetros a metros
 
-"""
-peso_kilogramos=float(input("ingresa tu peso en kilógramos"))
-estatura = float(input("ingresa tu estatura en cm"))
-estatura = estatura /100 #convirtiendo centímetros a metros
-"""
+imc = kilogramos /(estatura * estatura)
+print(f"Tu IMC es de {round(imc,2)} [Kg/m2]")
 
-import sys
-peso_kilogramos= sys.argv[1]
-estatura = sys.argv[2]
-
-print(sys.argv)
-print(sys.argv[1])
-print(sys.argv[2])
+if imc < 18.5:
+    print("Clasificación OMS : Bajo peso")
+elif imc < 25:
+    print("Clasificación OMS : Adecuado")
+elif imc < 30:
+    print("Clasificación OMS : Sobrepeso")
+elif imc < 35:
+    print("Clasificación OMS : Obesidad grado I")
+elif imc < 40:
+    print("Clasificación OMS : Obesidad grado II")
+else:
+    print("Clasificación OMS : Obesidad grado III")
